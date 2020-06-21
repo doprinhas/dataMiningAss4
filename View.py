d425e3748f2435d87a97c549d96b0c877422b15f
Freimans <freimans@post.bgu.ac.il>
2020-06-21 17:18:11 +0300

    graphs and input checks added

--- View.py
+++ View.py
@@ -1,19 +1,25 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image, ImageTk
 
 
 class View:
 
     def __init__(self, master, controller):
+        """ Constructor for View Class """
+
         self.controller = controller
         self.master = master
+        self.filename = ''
         master.title("K-Means Clustering")
-        master.minsize(1280, 800)
+        master.minsize(300, 200)
 
         vcmd = master.register(self.validate)
 
         self.browse_ent = Entry(master, text="Open File")
+        self.browse_ent.config(state=DISABLED)
         self.browse_btn = Button(master, text="Browse", command=self.file_dialog)
 
         self.num_of_clusters_ent = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
@@ -82,21 +88,58 @@ class View:
     def file_dialog(self):
         self.filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetype= \
             [("Excel file", "*.xlsx")])
+        self.browse_ent.config(state=NORMAL)
         self.browse_ent.delete(0, END)
         self.browse_ent.insert(0, self.filename)
+        self.browse_ent.config(state=DISABLED)
 
     def pre_process(self):
         try:
+            if self.filename == '':
+                messagebox.showerror("Error", "Path to data is invalid")
+                return
+
             self.controller.pre_process(self.filename)
-        except:
+            messagebox.showinfo("Finished!", "Preprocessing completed successfully! ")
+        except ValueError as err:
+            messagebox.showerror('Error!', str(err))
+            return
+        except Exception as err:
+            print(err)
             return
 
     def cluster(self):
-        if self.num_of_clusters_ent.get() == '' or self.num_of_iterations_ent.get() == '':
+        try:
+            if self.controller.proc_data is None:
+                messagebox.showerror("Error!", "Data is not processed")
+                return
+            if self.num_of_clusters_ent.get() == '' or self.num_of_iterations_ent.get() == '':
+                messagebox.showerror('Error!', 'You must fill all of the fields')
+                return
+            clusters_num = int(self.num_of_clusters_ent.get())
+            iterations_num = int(self.num_of_iterations_ent.get())
+            if clusters_num < 2 or clusters_num > len(self.controller.proc_data.index):
+                messagebox.showerror('Error!', 'The number of clusters is not valid')
+                return
+            if iterations_num < 1 or iterations_num > 100:
+                messagebox.showerror('Error!', 'The number of iterations is not valid')
+
+            graphs = self.controller.cluster(clusters_num, iterations_num)
+
+            self.master.geometry("1280x700")
+
+            graph1 = ImageTk.PhotoImage(Image.open(graphs[0]))
+            graph2 = ImageTk.PhotoImage(Image.open(graphs[1]))
+            graph_lbl1 = Label(self.master, image=graph1)
+            graph_lbl2 = Label(self.master, image=graph2)
+            graph_lbl1.place(relx=0.0, rely=1.0, anchor='sw')
+            graph_lbl2.place(relx=1.0, rely=1.0, anchor='se')
+
+            messagebox.showinfo("Finished!", "Clustering finished successfully")
+
+        except Exception as err:
+            print(err)
             return
-        clusters_num = int(self.num_of_clusters_ent.get())
-        iterations_num = int(self.num_of_iterations_ent.get())
-        self.controller.cluster(clusters_num, iterations_num)
 
     @property
     def controller(self):
--- ViewController.py
+++ ViewController.py
@@ -1,23 +1,57 @@
 import pandas as pd
 from PreProcessing import pre_processing
+import plotly.express as plex
+import chart_studio.plotly as pltly
 from Clustering import cluster_k_means
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class ViewController:
 
     def __init__(self):
-        pass
+        self.proc_data = None
 
     def pre_process(self, path):
         self.data = pd.read_excel(path)
+        if len(self.data.index) == 0:
+            raise ValueError("The given data is empty")
         self.proc_data = pre_processing(self.data, group_by_col='country')
         self.proc_data = self.proc_data.drop(['year'], axis=1)
-        print('finish pre processing: ', self.proc_data)
+        self.path = path
 
     def cluster(self, n_clusters=8, n_init=10):
         self.k_means = cluster_k_means(self.proc_data.drop('country', axis=1), n_clusters, n_init)
         self.proc_data['cluster'] = self.k_means.labels_
-        print('finish cluster: ', self.proc_data)
+        return self.get_graphs(self.k_means)
+
+    def get_graphs(self, k_means):
+        # First graph prep
+        fig, ax = plt.subplots()
+        sc = ax.scatter(self.proc_data['Social support'].values, self.proc_data['Generosity'].values, c=k_means.labels_.astype(np.float), edgecolor='k')
+        fig.colorbar(sc, ax=ax)
+
+        ax.set_xlabel('Social support', fontsize=12)
+        ax.set_ylabel('Generosity', fontsize=12)
+        fig.suptitle('K-Means clustering: Generosity - Social support', fontsize=12, fontweight='bold')
+        dir_path = self.path[:self.path.rfind('/')]
+        first_path = dir_path + '/Graph1.png'
+        plt.savefig(first_path)
+        plt.close(fig)
+
+        # Second graph prep
+        countries_data = pd.read_csv('countries_codes.csv')
+        countries_dict = dict([(country, code) for country, code in
+                               zip(countries_data['Country'].to_numpy(), countries_data['Alpha-3code'].to_numpy())])
+        self.proc_data['country id'] = [countries_dict[x] if x in countries_dict else x for x in self.proc_data['country'].values]
+
+        choro_map = plex.choropleth(self.proc_data, locations="country id", color="cluster",
+                                  color_continuous_scale=plex.colors.sequential.Aggrnyl,
+                                  title='K-Means clustering by countries')
+        pltly.sign_in('sfreiman', 'tQTEO9EOznyYp5HEu5Or')
+        second_path = dir_path + '/Graph2.png'
+        pltly.image.save_as(choro_map, filename=second_path)
+        return first_path, second_path
 
     @property
     def data(self):
